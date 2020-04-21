7f44f4818c27612f57cd21c6e6be600efbfac33d
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-04-21 09:09:33 +0000

    property binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,7 +1,34 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>{{ "Title: " + getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan"></td>
+            </tr>
+        </table>
+        <img [src]="imageUrl" />
+    `
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+    private title = "List of courses";
+    courses;
+    imageUrl = "./assets/beatles.png";
+    colSpan = 2;
+
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
+
+    getTitle() {
+        return this.title;
+    }
+}
