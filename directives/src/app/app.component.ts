8d653087de2ab8bee73548c164fbd83b56e2f48b
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-04-23 08:48:25 +0000

    ngStyle and ngClass

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -7,5 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // courses = [1, 2];
-  viewMode="map";
+  // viewMode="map";
+  courses;
+  idCount = 4;
+  onAdd() {
+    let tmpName: string = 'course' + this.idCount.toString();
+    this.courses.push({ id: this.idCount, name: tmpName })
+    this.idCount += 1;
+  }
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+  onInfect(course) {
+    let index = this.courses.indexOf(course);
+    this.courses[index].name = 'INFECTED';
+  }
+  loadCourses() {
+    this.courses = [
+      { id: 1, name: 'course1' },
+      { id: 2, name: 'course2' },
+      { id: 3, name: 'course3' },
+    ];
+  }
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
+  }
+
+  canSave = true;
+
+  task = {
+    title: 'Review applications',
+    assignee: {
+      name: 'John Smith'
+    }
+  }
 }
