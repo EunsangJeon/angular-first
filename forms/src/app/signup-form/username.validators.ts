07d84df2dc6a12ddaa1918d22ef60cf01fb27256
Eunsang <nicolaus1207@gmail.com>
2020-04-26 18:25:48 +0900

    async validation

--- forms/src/app/signup-form/username.validators.ts
+++ forms/src/app/signup-form/username.validators.ts
@@ -1,9 +1,25 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export class UsernameValidators {
-    static cannotContainSpace(control: AbstractControl) : ValidationErrors | null{ 
+    static cannotContainSpace(control: AbstractControl) : ValidationErrors | null{  
         if((control.value as string).indexOf(' ') >= 0)
             return { cannotContainSpace: true };
         return null;
+    }
+
+    static shouldBeUnique(control: AbstractControl) : Promise<ValidationErrors | null> {
+        return new Promise((resolve, reject) => {
+            // to simulate server latency
+            setTimeout(() => {
+                if(control.value === 'nick'){
+                    console.log('name is nick');
+                    resolve({ shouldBeUnique: true});   
+                }
+                else {
+                    console.log('name is not nick');
+                    resolve(null);
+                }
+            }, 2000);
+        });
     }
 }
