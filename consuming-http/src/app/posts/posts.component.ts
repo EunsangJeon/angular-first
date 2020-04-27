a139b915bc7ec9605a7e40698a773e6e9b706f6c
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-04-27 04:43:11 +0000

    CRUD http POST GET PUT(PATCH) DELETE

---
+++ consuming-http/src/app/posts/posts.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent {
+  posts: any[];
+  private url = 'http://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) {
+    http.get(this.url)
+      .subscribe(response => {
+        this.posts = response as any;
+      });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = {
+      title: input.value
+    }
+    input.value ='';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response['id'];
+        this.posts.splice(0, 0, post);
+        console.log(response)
+      });
+  }
+
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post.id , JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response);
+      });
+    // this.http.put(this.url, JSON.stringify(post));
+  }
+
+  deletePost(post) {
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe(reponse => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index ,1);
+      });
+  }
+}
