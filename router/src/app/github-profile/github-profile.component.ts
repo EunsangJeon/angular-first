ec4a22fdc1b1b9c40e9982c62a278ab70d4b5f31
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-04-28 00:49:15 +0000

    router finished

--- router/src/app/github-profile/github-profile.component.ts
+++ router/src/app/github-profile/github-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { __core_private_testing_placeholder__ } from '@angular/core/testing';
 
 @Component({
   selector: 'app-github-profile',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-profile.component.css']
 })
 export class GithubProfileComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    // if parameter will never change you can consider:
+    // this.route.snapshot.paramMap
+
+    this.route.paramMap
+      .subscribe(params => {
+        let id = +params.get('id');
+        console.log(id);
+      })
   }
 
+  submit() {
+    this.router.navigate(['/followers', {
+      queryParams: { page: 1, order: 'newest'}
+    }]);
+  }
 }
